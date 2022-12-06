9298a34cfb1200900a27f2af2e5ed118047fc648
Milton Martins <milton.fidelis@enterscience.com.br>
2022-12-05 23:17:03 -0300

    .

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,27 @@
 //* Libraries imports
 import { Route, Routes } from "react-router-dom";
+import { Footer } from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+import Header from "../views/LandingPage/Header";
 
 //* Routes imports
 import LandingPage from "../views/LandingPage/LandingPage";
 import ProductView from "../views/ProductView/ProductView";
 
 const RoutesContainer = () => (
-  <Routes>
-    <Route path="/" element={<LandingPage />} />
-    <Route path="/product/:id" element={<ProductView />} />
-  </Routes>
+  <div className="flex flex-col mt-5">
+    <Header />
+    
+    <Navbar />
+
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/:category_param" element={<LandingPage />} />
+      <Route path="/product/:id" element={<ProductView />} />
+    </Routes>
+
+    <Footer />
+  </div>
 );
 
 export default RoutesContainer;
--- src/views/LandingPage/ProductList/index.jsx
+++ src/views/LandingPage/ProductList/index.jsx
@@ -8,14 +8,23 @@ import ProductCard from "../../../components/ProductCard/ProductCard";
 
 const responsive = {
   0: {
+    items: 1,
+  },
+  580: {
     items: 2,
   },
-  500: {
+  740: {
     items: 3,
   },
-  1000: {
+  900: {
     items: 4,
-  }
+  },
+  1030: {
+    items: 2,
+  },
+  1360: {
+    items: 3,
+  },
 };
 
 const landing_page_style = [
@@ -26,10 +35,11 @@ const landing_page_style = [
   "font-bold",
 ];
 
-const ProductList = ({ products, color_palette }) => {
+const ProductList = ({ products, color_palette, category_param }) => {
   return (
-    <div className="flex flex-col gap-10">
+    <div className="flex flex-col gap-10 min-h-screen">
       {Object.keys(products).map((category, index) => {
+        if (category_param && category_param !== category) return null;
         return (
           <div
             className={landing_page_style.join(" ")}
@@ -38,10 +48,11 @@ const ProductList = ({ products, color_palette }) => {
               backgroundColor: color_palette[index]
                 ? color_palette[index]
                 : color_palette[index / color_palette.length],
+              boxShadow: "inset 0 0 0 0.4rem white",
             }}
           >
             {/* first letter uppercase */}
-            <p className="text-2xl mb-[0.85rem] px-8 py-2 bg-white w-max rounded-r-xl">
+            <p className="text-2xl mb-[0.25rem] px-8 py-2 bg-white w-full text-center rounded-r-xl">
               {category.charAt(0).toUpperCase() + category.slice(1)}
             </p>
 
@@ -52,13 +63,10 @@ const ProductList = ({ products, color_palette }) => {
                 dots={false}
                 nav={false}
                 center
-                autoplay={true}
-                autoplayTimeout={6000}
+                // autoplay={true}
+                // autoplayTimeout={6000}
                 responsive={responsive}
-                stagePadding={0}
-                cellPadding={0}
-                margin={0}
-                lazyLoad={true}
+                stagePadding={100}
               >
                 {products[category].map((product) => {
                   return <ProductCard key={product.id} product={product} />;
